6cd183920b1e92d961913bad1d0ea1c700c97855
boldirev-as <wot.1106@yandex.ru>
2022-12-08 17:25:21 +0500

    improved speed

--- test_diff_img.py
+++ test_diff_img.py
@@ -11,11 +11,14 @@ from imutils.object_detection import non_max_suppression
 from flask import Flask, request
 from flask_restful import Api
 import json
+import fastwer
 
 app = Flask(__name__)
 api = Api(app)
 
 net = cv2.dnn.readNet("frozen_east_text_detection.pb")
+blank = cv2.imread('blank.png')
+blank = cv2.resize(blank, (25, 50), interpolation=cv2.INTER_AREA)
 
 
 def my_dist(a, b):
@@ -65,18 +68,24 @@ def improve_image(image, size=100, chan=125):
 
 
 def get_str_from_img_with_detector(img):
+    t = time.time()
     orig = improve_image(img)
+    print("IMPROVED", time.time() - t)
 
-    blob = cv2.dnn.blobFromImage(img, 1.0, (960, 960), (123.68, 116.78, 103.94), True, False)
+    CONV_X, CONV_Y = 960, 960
 
-    outputLayers = []
-    outputLayers.append("feature_fusion/Conv_7/Sigmoid")
-    outputLayers.append("feature_fusion/concat_3")
+    t = time.time()
+    blob = cv2.dnn.blobFromImage(img, 1.0, (CONV_X, CONV_Y), (123.68, 116.78, 103.94), True, False)
+    print("BLOB", time.time() - t)
 
+    outputLayers = ["feature_fusion/Conv_7/Sigmoid", "feature_fusion/concat_3"]
+
+    t = time.time()
     net.setInput(blob)
     output = net.forward(outputLayers)
     scores = output[0]
     geometry = output[1]
+    print("FORWARD", time.time() - t)
 
     (numRows, numCols) = scores.shape[2:4]
     rects = []
@@ -108,34 +117,61 @@ def get_str_from_img_with_detector(img):
             rects.append((startX, startY, endX, endY))
             confidences.append(scoresData[x])
 
+    t = time.time()
     boxes = non_max_suppression(np.array(rects), probs=confidences)
-    output = []
 
-    rW, rH = orig.shape[1] / 960, orig.shape[0] / 960
+    rW, rH = orig.shape[1] / CONV_X, orig.shape[0] / CONV_Y
+    t_sum2 = 0
+    t_sum = 0
+    whole_text = blank
     for (startX, startY, endX, endY) in boxes:
-        if 0.9 <= (endX - startX) / (endY - startY) <= 1.6:
-            continue
+        # if 0.9 <= (endX - startX) / (endY - startY) <= 1.6:
+        #    continue
         startX = int(startX * rW)
         startY = int(startY * rH)
         endX = int(endX * rW)
-        endY = int(endY * rH) + 15
+        endY = int(endY * rH) + 25
+
         cropped = crop_img_by_polygon(orig, ((startX, startY), (endX, startY),
                                              (endX, endY), (startX, endY)))
         cropped2 = crop_img_by_polygon(img, ((startX, startY), (endX, startY),
                                              (endX, endY), (startX, endY)))
         try:
-            surname = pytesseract.image_to_string(cropped, config="--psm 8").strip("\n")
-            surname = re.sub(r"[^A-Za-z]", "", surname)
-            output.append(surname)
+            # t2 = time.time()
+            # cropped = cv2.resize(cropped, (100, 50), interpolation=cv2.INTER_AREA)
+            # surname = pytesseract.image_to_string(cropped, config="--psm 8").strip("\n")
+            # surname = re.sub(r"[^A-Za-z]", "", surname)
+            # output.append(surname)
+            # t_sum2 += time.time() - t2
+            # cv2.imwrite(f"bin/{surname}.png", cropped)
 
-            surname = pytesseract.image_to_string(cropped2, config="--psm 8").strip("\n")
-            surname = re.sub(r"[^A-Za-z]", "", surname)
-            output.append(surname)
+            # surname = pytesseract.image_to_string(cropped2, config="--psm 8").strip("\n")
+            # surname = re.sub(r"[^A-Za-z]", "", surname)
+            # output.append(surname)
+
+            t2 = time.time()
+            whole_text = np.concatenate((whole_text,
+                                         blank,
+                                         cv2.resize(cropped, (cropped.shape[0], 50), interpolation=cv2.INTER_AREA),
+                                         blank,
+                                         cv2.resize(cropped2, (cropped2.shape[0], 50), interpolation=cv2.INTER_AREA)),
+                                        axis=1)
+            t_sum += time.time() - t2
         except TypeError as e:
             print(e)
             continue
-
-        # cv2.rectangle(img, (startX, startY), (endX, endY), (0, 255, 0), 2)
+        cv2.rectangle(img, (startX, startY), (endX, endY), (0, 255, 0), 2)
+        # show the output image
+    cv2.imwrite("ans.png", img)
+    # cv2.imwrite("whole_text.png", whole_text)
+    t2 = time.time()
+    surname = pytesseract.image_to_string(whole_text, config="--psm 7").strip("\n")
+    t_sum += time.time() - t2
+    # print(re.sub(r"[^A-Za-z ]", "", surname).split())
+    # print(output)
+    print("BOX TO STR", time.time() - t, "CROPPED", t_sum2, "NEW", t_sum)
+    output = re.sub(r"[^A-Za-z ]", "", surname).split()
+    # cv2.rectangle(img, (startX, startY), (endX, endY), (0, 255, 0), 2)
     # show the output image
     # cv2.imwrite("ans.png", img)
     return [x for x in output if len(x) > 2]
@@ -146,7 +182,6 @@ def get_surnames_from_img(img):
     probe_surnames = re.sub(r"[^A-Za-z-]", " ", text).split()
 
     probe_surnames = [surname for surname in probe_surnames if len(surname) >= 3]
-    print(probe_surnames)
 
     return probe_surnames
 
@@ -155,7 +190,14 @@ def get_surnames_from_img(img):
 # print(get_str_from_img_with_detector(cv2.imread("images/16.png")))
 
 
-def get_all_possible_surnames(img):
+def form_surnames(candidates):
+    candidates = ["".join([x for x in surname if re.match(r"[A-Za-z]", x)]) for surname in candidates]
+    candidates = [surname.lower() for surname in candidates if len(surname) >= 3]
+    candidates = list(set(candidates))
+    return candidates
+
+
+def get_all_possible_surnames(img, main=False, surnames=None):
     candidates = []
 
     scale_percent = 400
@@ -164,15 +206,31 @@ def get_all_possible_surnames(img):
     dim = (width, height)
     img = cv2.resize(img, dim, interpolation=cv2.INTER_AREA)
 
+    t = time.time()
     candidates.extend(get_surnames_from_img(img))
+    print("1 cand", time.time() - t)
+    if max([x[1] for x in choose_the_most_suitable_candidates(candidates, surnames)[:(11 if main else 9)]]) <= 30:
+        candidates = form_surnames(candidates)
+        print(candidates)
+        return candidates
 
+    t = time.time()
     candidates.extend(get_surnames_from_img(improve_image(img)))
+    print("2 cand", time.time() - t)
+    if max([x[1] for x in choose_the_most_suitable_candidates(candidates, surnames)[:(11 if main else 9)]]) <= 30:
+        candidates = form_surnames(candidates)
+        print(candidates)
+        return candidates
 
-    candidates.extend((get_str_from_img_with_detector(img)))
+    if main:
+        t = time.time()
+        candidates.extend((get_str_from_img_with_detector(img)))
+        print("3 cand", time.time() - t)
 
     candidates = ["".join([x for x in surname if re.match(r"[A-Za-z]", x)]) for surname in candidates]
-    candidates = [surname for surname in candidates if len(surname) >= 3]
-
+    candidates = [surname.lower() for surname in candidates if len(surname) >= 3]
+    candidates = list(set(candidates))
+    print(candidates)
     return candidates
 
 
@@ -180,25 +238,29 @@ def choose_the_most_suitable_candidates(candidates, surnames):
     t = time.time()
     probe_surnames_scores = []
     for prob_surname in candidates:
+        min_cer = 1000
+        best_surname = ""
         for surname in surnames:
             if len(surname) == 0:
                 continue
-            cer = my_dist(prob_surname.lower(), surname.lower().split()[-1])
-            if cer >= len(surname):
-                continue
-            probe_surnames_scores.append([surname, cer])
+            cer = fastwer.score_sent(prob_surname.lower(), surname.lower().split()[-1], char_level=True)
+            if cer < min_cer:
+                min_cer = cer
+                best_surname = surname
             if cer == 0:
                 break
+        probe_surnames_scores.append([best_surname, min_cer])
+
     probe_surnames_scores.sort(key=lambda x: x[1])
     print("SCORED DURING", time.time() - t)
     return probe_surnames_scores
 
 
-def answer_for_img(img, coords, surnames, num_players):
+def answer_for_img(img, coords, surnames, num_players, main=False):
     cropped_img = crop_img_by_polygon(img, np.array([(coords[i], coords[i + 1])
                                                      for i in range(0, len(coords), 2)]))
-    main_players = choose_the_most_suitable_candidates(get_all_possible_surnames(cropped_img), surnames)
-    print("players founded", len(main_players))
+    main_players = choose_the_most_suitable_candidates(get_all_possible_surnames(cropped_img, main, surnames), surnames)
+
     top_players = set()
     scores = []
     i = 0
@@ -214,6 +276,7 @@ def answer_for_img(img, coords, surnames, num_players):
 @app.route('/api/image', methods=['POST'])
 def upload():
     t = time.time()
+
     original_image = request.files['image']
     original_image.save("input.png")
 
@@ -222,8 +285,6 @@ def upload():
         input[item] = request.form[item].replace("\\'", "")
         if item == "team":
             input[item] = json.loads(input[item])
-
-    print(input)
 
     original_img = cv2.imread("input.png")
     team_members = [player["full_name"] for player in input["team"]]
@@ -237,15 +298,15 @@ def upload():
         if input[state[0]] == "":
             continue
         coords = list(map(int, input[state[0]].split(",")))
-        scores_main, main_players = answer_for_img(original_img, coords, team_members, 11)
+        scores_main, main_players = answer_for_img(original_img, coords, team_members, 11, True)
 
         if input[state[1]] == "":
             continue
         coords = list(map(int, input[state[1]].split(",")))
         scores_support, support_players = answer_for_img(original_img, coords, team_members, 9)
 
-    main_recognized = sum([1 for x in scores_main if x <= 2])
-    support_recognized = sum([1 for x in scores_support if x <= 2])
+    main_recognized = sum([1 for x in scores_main if x <= 50])
+    support_recognized = sum([1 for x in scores_support if x <= 50])
 
     print((time.time() - t))
 
@@ -259,5 +320,6 @@ def upload():
 
 
 if __name__ == "__main__":
-    from witress import serve
-    serve(app, host="0.0.0.0", port=3000)
+    # from waitress import serve
+    # serve(app, host="0.0.0.0", port=3000)
+    app.run(debug=True)
